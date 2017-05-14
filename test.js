1a6db7e50f7e644a0a055fd8873a4c5e1e4e0d95
0yukikaze <neothedefender@gmail.com>
2017-05-15 00:28:02 +0530

    Added docker exec example

--- test.js
+++ test.js
@@ -8,10 +8,7 @@ function test(){
     //docker.containers.getAllContainers().then((result) => console.log(result))
     //docker.network.getNetworkListing().then((result) => console.log(result));
     //docker.images.buildImage('xyz/pqr:1.0', 'https://github.com/0yukikaze0/Quorum-Workspace/blob/master/Docker/').then((response) => console.log(response), (err) => console.log(err))
-    //docker.exec.execProcess('My_New_Test_Container', ['/bin/bash', '-sh', '"touch /data/xyz/hello"'])
-    //docker.exec.execProcess('My_New_Test_Container', ['/bin/bash', '-sh', 'touch /data/xyz/hello'])
-    //docker.exec.execProcess('My_New_Test_Container', ['/bin/bash', '-c "touch /data/xyz/hellothere"'])
-    docker.exec.execProcess('My_New_Test_Container', ['/bin/bash', '-c', 'echo sq > /data/xyz/xhello'])
+    docker.exec.execProcess('My_New_Test_Container', ['/bin/bash', '-c', 'echo "klaatu barada nikto" > /data/xyz/message'])
         .then( (response) => console.log(response),
                 (err) => console.log(err));
 }
