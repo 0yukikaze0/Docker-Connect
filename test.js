5ec7252eb3038a20a755f5cb5c3fb56eae4ead07
0yukikaze <neothedefender@gmail.com>
2017-05-14 22:48:09 +0530

    Added examples

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
-var Docker = require('./index');
+var Docker = require('./DockerConnect');
 
 function test(){
 
     let docker = new Docker('/var/run/docker.sock');
 
-    docker.images.getAllImages().then( (result) => console.log(result))
-    docker.containers.getAllContainers().then((result) => console.log(result))
-    docker.network.getNetworkListing().then((result) => console.log(result));
+    //docker.images.getAllImages().then( (result) => console.log(result))
+    //docker.containers.getAllContainers().then((result) => console.log(result))
+    //docker.network.getNetworkListing().then((result) => console.log(result));
+    //docker.images.buildImage('xyz/pqr:1.0', 'https://github.com/0yukikaze0/Quorum-Workspace/blob/master/Docker/').then((response) => console.log(response), (err) => console.log(err))
 }
 
 test();
